3e6dd64aa674efce6307404554e4d865940148ee
AjinkyaHon9504 <143346648+AjinkyaHon9504@users.noreply.github.com>
2025-07-27 18:58:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (42.56%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long long N = n; // To safely handle INT_MIN
-
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
+        long long N = n;
+        
+        //base case
+        if(n==0){
+            return 1;
         }
 
-        return power(x, N);
+        if(N<0){
+           
+            x=1/x;
+             N=-N;
+        }
+
+        return power(x,N);
     }
 
     double power(double x, long long n) {
-        if (n == 0) return 1;
-        if (n == 1) return x;
 
-        double half = power(x, n / 2);
-
-        if (n % 2 == 0)
-            return half * half;
-        else
+        //base case
+         if(n==0){
+            return 1;
+        }
+         if(n==1){
+            return x;
+        }
+        int p = n/2;
+        
+        double half = power(x,p);
+        if(n%2 == 0){  //even 
+        return half * half;
+        } 
+        else{
             return half * half * x;
+        }
+
     }
+
+    //divide and conquer
+
+
 };
