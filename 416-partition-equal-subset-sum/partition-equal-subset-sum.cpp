8b7656602b2eed4c4b7e008c293980ac00768e53
AjinkyaHon9504 <143346648+AjinkyaHon9504@users.noreply.github.com>
2026-04-18 12:36:52 +0530

    Time: 135 ms (46.92%) | Memory: 17.5 MB (52.97%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,48 +1,44 @@
 class Solution {
 public:
 
+    bool subsetsum(vector<int> &nums,int sum){
+        int n = nums.size();
 
-    bool subsetsum(vector<int>&nums,int target){
-        int n=nums.size();
+        vector<vector<bool>> dp(n+1,
+                                vector<bool>(sum+1,false));
 
-        bool dp[n+1][target+1];
-        for(int i=0;i<=n;i++){
-            dp[i][0] = true;
-        }
+        // Base case
+        for(int i=0;i<=n;i++)
+            dp[i][0]=true;
 
-        for(int j=1;j<=target;j++){
-            dp[0][j] = false;
-        }
-
+        // Fill DP table
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=target;j++){
-                if(nums[i-1]<=j){
-                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+            for(int j=1;j<=sum;j++){
+
+                if(nums[i-1] <= j){
+                    dp[i][j] =
+                        dp[i-1][j-nums[i-1]]
+                        || dp[i-1][j];
                 }
                 else{
                     dp[i][j] = dp[i-1][j];
                 }
             }
         }
-        return dp[n][target];
 
-        
+        return dp[n][sum];
     }
-    bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int sum =0;
-        for(int i=0;i<n;i++){
-            sum=sum+nums[i];
-        }
-        int target = sum/2;
-        if(sum%2!=0){
-            return false;
-        }
-        return subsetsum(nums,target);
-        
 
-        }
-        
-        
-    
+    bool canPartition(vector<int>& nums) {
+
+        int sum = 0;
+
+        for(int x: nums)
+            sum += x;
+
+        if(sum % 2 != 0)
+            return false;
+
+        return subsetsum(nums, sum/2);
+    }
 };
