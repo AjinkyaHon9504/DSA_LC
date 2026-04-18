f835340d9d3c58c9b615d293dc2ad0b65e4cb7e9
AjinkyaHon9504 <143346648+AjinkyaHon9504@users.noreply.github.com>
2026-04-18 16:21:35 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (39.45%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -2,38 +2,33 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        int range = 0;
-
-        for(int i = 0; i < n; i++){
-            range += nums[i];
+        int totalsum=0;
+        for(int i=0;i<n;i++){
+            totalsum+=nums[i];
         }
 
-        if (abs(target) > range || (target + range) % 2 != 0)
+        if(abs(target)>totalsum || (target + totalsum)%2 !=0){
             return 0;
+        }
 
-        int sum = (target + range) / 2;
+        int Psum = (target+totalsum)/2;
 
-        int dp[n+1][sum+1];
+        vector<vector<int>> dp(n+1,vector<int>(Psum+1,0));
+        dp[0][0]=1;
 
-        // Base cases
-        for(int i = 0; i <= n; i++)
-            dp[i][0] = 1;
+        
 
-        for(int j = 1; j <= sum; j++)
-            dp[0][j] = 0;
-
-        // Fill DP
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= sum; j++) {
-                if (nums[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j];
-                } 
-                else {
-                    dp[i][j] = dp[i - 1][j];
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=Psum;j++){
+                if(nums[i-1]<=j){
+                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
                 }
             }
-        }
 
-        return dp[n][sum];
+        }
+        return dp[n][Psum];
     }
 };
