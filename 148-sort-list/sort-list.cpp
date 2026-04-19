07ab17e0d2c051b48fa272666e9d89c273abae4f
AjinkyaHon9504 <143346648+AjinkyaHon9504@users.noreply.github.com>
2026-04-19 17:35:04 +0530

    Time: 12 ms (72.68%) | Memory: 57.2 MB (72.87%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -1,56 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
 
-    // returns head of right half
-    ListNode* findMiddle(ListNode* head){
-        ListNode* slow = head;
-        ListNode* fast = head->next;
+    ListNode* middle(ListNode * head){
+        ListNode *slow=head;
+        ListNode *fast= head->next;
 
-        while(fast && fast->next){
-            slow = slow->next;
-            fast = fast->next->next;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-
-        ListNode* mid = slow->next;
-        slow->next = NULL; // split here
-        return mid;
+        return slow;
     }
 
-    ListNode* merge(ListNode* head1, ListNode* head2){
-        ListNode* t1 = head1;
-        ListNode* t2 = head2;
+    ListNode* merge(ListNode *l1,ListNode* l2){
 
         ListNode dummy(-1);
-        ListNode* temp = &dummy;
+        ListNode* tail = &dummy;
 
-        while(t1 && t2){
-            if(t1->val < t2->val){
-                temp->next = t1;
-                t1 = t1->next;
-            } else {
-                temp->next = t2;
-                t2 = t2->next;
+        while(l1 && l2){
+            if(l1->val < l2->val){
+                tail->next = l1;
+                l1=l1->next;
             }
-            temp = temp->next;
+            else{
+                tail->next = l2;
+                l2=l2->next;
+            }
+            tail=tail->next;
+
         }
 
-        if(t1) temp->next = t1;
-        else temp->next = t2;
+        if(l1) tail->next = l1;
+        if(l2) tail->next = l2;
 
         return dummy.next;
     }
-
     ListNode* sortList(ListNode* head) {
-        if(!head || !head->next) return head;
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
 
-        ListNode* mid = findMiddle(head);
+        ListNode *mid = middle(head);
+        ListNode *right = mid->next;
+        mid->next =NULL;
 
-        ListNode* left = head;
-        ListNode* right = mid;
+        ListNode *left = sortList(head);
+        ListNode * rightnode = sortList(right);
 
-        left = sortList(left);
-        right = sortList(right);
-
-        return merge(left, right);
+        return merge(left,rightnode);
     }
 };
