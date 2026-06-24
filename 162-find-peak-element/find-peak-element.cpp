d47be89a24c61cf9b7571246e86322684900bf38
AjinkyaHon9504 <143346648+AjinkyaHon9504@users.noreply.github.com>
2026-06-24 17:08:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (39.16%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -5,22 +5,17 @@ public:
 
         if (n == 1) return 0;
 
-        map<int,int> mp;
-
-        if (nums[0] > nums[1]) {
-            mp[nums[0]] = 0;
-        }
-
-        if (nums[n-1] > nums[n-2]) {
-            mp[nums[n-1]] = n-1;
-        }
-
-        for (int i = 1; i < n - 1; i++) {
-            if (nums[i] > nums[i-1] && nums[i] > nums[i+1]) {
-                mp[nums[i]] = i;
+        int low =0;
+        int high = n-1;
+        while(low<high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]>nums[mid+1]){
+                high=mid;
+            }
+            else{
+                low=mid+1;
             }
         }
-
-        return mp.rbegin()->second;
+        return low;
     }
 };
